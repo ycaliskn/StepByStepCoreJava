fe97288ae26dce6ee15dbabdb3c8c252cfc04bf7
Yunus Çalışkan <y.clsknnn@gmail.com>
2023-08-27 23:35:14 +0300

    Delete Homework directory

--- Homework/Ogrenci.java
+++
@@ -1,9 +0,0 @@
-package Homework;
-
-public class Ogrenci extends Okul{
-    public void yapmak(){
-        System.out.println("Ogrenci ders dinler.");
-    }
-    public int ogrenciSayisi=200;
-    int kitap=5;
-}
